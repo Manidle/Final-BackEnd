a2568f1bbf601ec5b5e21431a10773f373178da1
Glory <dudrhkd6550@naver.com>
2022-08-25 00:36:16 +0900

    초기 브랜치

---
+++ src/main/java/com/example/planergram/model/Post.java
@@ -0,0 +1,37 @@
+package com.example.planergram.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false,length = 30)
+    private String title;
+
+    @Lob //대용량 데이터
+    @Column(length = 30)
+    private String contents;
+
+    @Column(name = "like_count")
+    private int likeCount;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="board_id")
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="user_id")
+    private User user;
+}
