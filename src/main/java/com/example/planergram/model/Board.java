44eec23c078b75403d584e1dc45afe431563b016
Glory <dudrhkd6550@naver.com>
2022-08-24 19:29:06 +0900

    feat:model update&create complete

--- src/main/java/com/example/planergram/model/Board.java
+++ src/main/java/com/example/planergram/model/Board.java
@@ -16,13 +16,10 @@ public class Board {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(length = 100)
+    @Column(name = "board_title",length = 100)
     private String title;
 
     @Lob //대용량 데이터
-    private String content;
-
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name="userid")
-    private User user;
+    @Column(name = "board_img",length = 100)
+    private String img;
 }
--- src/main/java/com/example/planergram/model/User.java
+++ src/main/java/com/example/planergram/model/User.java
@@ -16,18 +16,14 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
     private int id;
 
-    @Column(name = "username", nullable = false,length = 30, unique = true)
-    private String username;
+    @Column(name = "user_login_id", nullable = false,length = 30, unique = true)
+    private String loginId;
 
     @Column(nullable = false,length = 100)
     private String password;
 
-    @Column(name = "nickname", nullable = false,length = 30, unique = true)
+    @Column(nullable = false,length = 30, unique = true)
     private String nickname;
-
-    @Column(nullable = false,length = 50)
-    private String email;
 }
