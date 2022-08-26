fb2e71156a556460f4671012390629d818dc8ce6
Yiseun <yiseunn@gmail.com>
2022-08-26 12:28:56 +0900

    feat:Train model 추가

---
+++ src/main/java/com/example/planergram/model/Train.java
@@ -0,0 +1,38 @@
+package com.example.planergram.model;
+
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class Train {
+
+    @Id
+    @Column(name="train_id",nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int trainId;
+
+    @Column(name = "departure_time")
+    private LocalDateTime departureTime;
+
+    @Column(name = "arrive_time")
+    private LocalDateTime arriveTime;
+
+    @Column(name = "start_point")
+    private String startPoint;
+
+    @Column(name = "end_point")
+    private String endPoint;
+
+    @Column(name = "train_price")
+    private int trainPrice;
+
+
+}
