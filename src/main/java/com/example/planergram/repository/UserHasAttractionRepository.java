646eb48919370c7fc014e32931111eea3ef905e7
Yiseun <yiseunn@gmail.com>
2022-08-29 03:56:34 +0900

    refactor:UserHasAttraction Repository 수정

--- src/main/java/com/example/planergram/repository/UserHasAttractionRepository.java
+++ src/main/java/com/example/planergram/repository/UserHasAttractionRepository.java
@@ -1,9 +1,17 @@
 package com.example.planergram.repository;
 
+import com.example.planergram.model.Train;
 import com.example.planergram.model.UserHasAttraction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserHasAttractionRepository extends JpaRepository<UserHasAttraction, Integer> {
+
+    List<UserHasAttraction> findByUser(@Param("userId") int userId);
+
+    List<UserHasAttraction> findByAttraction(@Param("attractionId") int attractionId);
 }
