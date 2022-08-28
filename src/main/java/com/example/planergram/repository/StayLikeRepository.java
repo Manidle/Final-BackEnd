65123c25bc5ccb86bf11dc45e1a8a55f432bdfbb
소울치킨 <soulfever01@naver.com>
2022-08-28 22:36:10 +0900

    setup : 필요한 repository, service 클래스 생성

---
+++ src/main/java/com/example/planergram/repository/StayLikeRepository.java
@@ -0,0 +1,7 @@
+package com.example.planergram.repository;
+
+import com.example.planergram.model.StayLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StayLikeRepository extends JpaRepository<StayLike,Integer> {
+}
