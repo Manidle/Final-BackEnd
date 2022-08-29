dd5413a96bd05728740ae9b55fec845789ff7ad5
Yiseun <yiseunn@gmail.com>
2022-08-30 02:24:09 +0900

    refactor:Train DTO 수정

--- src/main/java/com/example/planergram/DTO/TrainDTO.java
+++ src/main/java/com/example/planergram/DTO/TrainDTO.java
@@ -1,7 +1,13 @@
 package com.example.planergram.DTO;
 
+import com.example.planergram.model.Train;
+import lombok.Builder;
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Builder
+@Data
 public class TrainDTO {
     private Long trainId;
     private LocalDateTime departureTime;
@@ -9,4 +15,26 @@ public class TrainDTO {
     private String startPoint;
     private String endPoint;
     private int trainPrice;
+
+    private TrainDTO toTrainDTO(Train train){
+        return TrainDTO.builder()
+                .trainId(train.getTrainId())
+                .departureTime(train.getDepartureTime())
+                .arriveTime(train.getArriveTime())
+                .startPoint(train.getStartPoint())
+                .endPoint(train.getEndPoint())
+                .trainPrice(train.getTrainPrice())
+                .build();
+    }
+
+    private Train toTrain(TrainDTO trainDTO){
+        return  Train.builder()
+                .trainId(trainDTO.getTrainId())
+                .departureTime(trainDTO.getDepartureTime())
+                .arriveTime(trainDTO.getArriveTime())
+                .startPoint(trainDTO.getStartPoint())
+                .endPoint(trainDTO.getEndPoint())
+                .trainPrice(trainDTO.getTrainPrice())
+                .build();
+    }
 }
