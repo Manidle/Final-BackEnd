22830c8172145b058fb85438dea82207a13b5105
Yiseun <yiseunn@gmail.com>
2022-08-30 01:27:50 +0900

    feat:Attraction DTO 추가

---
+++ src/main/java/com/example/planergram/DTO/AttractionDTO.java
@@ -0,0 +1,9 @@
+package com.example.planergram.DTO;
+
+public class AttractionDTO {
+    private Long attractionId;
+    private String attractionName;
+    private String attractionAddress;
+    private String attractionAddressDetail;
+    private Long attractionPrice;
+}
