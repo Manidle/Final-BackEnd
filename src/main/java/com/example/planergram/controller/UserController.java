b828832762bd55353db4b5f88959f439e9f1744b
소울치킨 <soulfever01@naver.com>
2022-08-29 16:47:37 +0900

    chore : url 수정

--- src/main/java/com/example/planergram/controller/UserController.java
+++ src/main/java/com/example/planergram/controller/UserController.java
@@ -13,24 +13,24 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/join")
+    @PostMapping("/user")
     public String signUp(@RequestBody User user){
         userService.signup(user);
         return "회원가입이 완료되었습니다.";
     }
 
-    @GetMapping("/findall")
+    @GetMapping("/user")
     public List<User> findAll() {
         System.out.println("findAll called!");
         return userService.findAll();
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/user/{id}")
     public List<User> delete(@PathVariable Long id){
         return userService.delete(id);
     };
 
-    @PutMapping("/update/{id}")
+    @PutMapping("/user/{id}")
     public List<User> update(@PathVariable Long id ,@RequestBody User user) {
         userService.update(id,user);
         return userService.update(id,user);
