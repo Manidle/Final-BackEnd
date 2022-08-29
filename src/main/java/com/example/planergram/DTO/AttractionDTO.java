05795337faeffe3913b7d49fba8c17af36db9bb2
Yiseun <yiseunn@gmail.com>
2022-08-30 02:16:37 +0900

    refactor:Attraction DTO 수정

--- src/main/java/com/example/planergram/DTO/AttractionDTO.java
+++ src/main/java/com/example/planergram/DTO/AttractionDTO.java
@@ -1,9 +1,39 @@
 package com.example.planergram.DTO;
 
+import com.example.planergram.model.Attraction;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Builder
+@Data
 public class AttractionDTO {
     private Long attractionId;
     private String attractionName;
     private String attractionAddress;
     private String attractionAddressDetail;
-    private Long attractionPrice;
+    private int attractionPrice;
+
+    private AttractionDTO toAttractionDTO(Attraction attraction){
+        return AttractionDTO.builder()
+                .attractionId(attraction.getAttractionId())
+                .attractionName(attraction.getAttractionName())
+                .attractionAddress(attraction.getAttractionAddress())
+                .attractionAddressDetail(attraction.getAttractionAddressDetail())
+                .attractionPrice(attraction.getAttractionPrice())
+                .build();
+    }
+
+    private Attraction showAttraction(AttractionDTO attractionDTO){
+        return Attraction.builder()
+                .attractionId(attractionDTO.getAttractionId())
+                .attractionName(attractionDTO.getAttractionName())
+                .attractionAddress(attractionDTO.getAttractionAddress())
+                .attractionAddressDetail(attractionDTO.getAttractionAddressDetail())
+                .attractionPrice(attractionDTO.getAttractionPrice())
+                .build();
+    }
+
 }
+
