2240e284fe23653c75fa93a9ce2672c64911d3fc
Yiseun <yiseunn@gmail.com>
2022-08-30 16:34:43 +0900

    refactor:Attraction controller 수정

--- src/main/java/com/example/planergram/controller/AttractionController.java
+++ src/main/java/com/example/planergram/controller/AttractionController.java
@@ -1,28 +1,46 @@
 package com.example.planergram.controller;
 
+import com.example.planergram.DTO.AttractionDTO;
 import com.example.planergram.model.Attraction;
 import com.example.planergram.model.Board;
 import com.example.planergram.service.AttractionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class AttractionController {
 
+
     @Autowired
     private AttractionService attractionService;
 
-    @GetMapping("/attraction/{attraction_id}")
-    public List<Attraction> findAll() {
-        return attractionService.findAll();
+    @GetMapping("/attraction")
+    public List<AttractionDTO> findAll() {
+        return attractionService.getAttractionList();
     }
 
-    @GetMapping("/attraction/{attraction_address}")
-    public List<Attraction> findByAddress(){
-        return attractionService.findSeletedRegion();
+    //보류
+//    @GetMapping("/attraction/{attraction_address}")
+//    public List<Attraction> findByAddress(){
+//        return attractionService.findSeletedRegion();
+//    }
+
+    @DeleteMapping("/attraction/delete/{attraction_id}")
+    public void deleteById(Long attractionId) {
+        attractionService.deleteAttraction(attractionId);
+    }
+
+    @PostMapping("/attraction/regist")
+    public void regist(AttractionDTO attractionDTO){
+        attractionService.createAttraction(attractionDTO);
+    }
+
+    @PostMapping("/attraction/edit/{attraction_id}")
+    public Attraction edit(AttractionDTO attractionDTO){
+        return attractionService.editAttraction(attractionDTO);
     }
 
 
