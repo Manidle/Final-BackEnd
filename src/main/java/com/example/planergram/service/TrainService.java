633c4ff3970f81d99e5b52c399e7e9728f975957
Yiseun <yiseunn@gmail.com>
2022-08-30 17:00:25 +0900

    refactor:Train service 수정 전체조회 수정

--- src/main/java/com/example/planergram/service/TrainService.java
+++ src/main/java/com/example/planergram/service/TrainService.java
@@ -1,10 +1,14 @@
 package com.example.planergram.service;
 
+import com.example.planergram.DTO.AttractionDTO;
+import com.example.planergram.DTO.TrainDTO;
 import com.example.planergram.model.Attraction;
 import com.example.planergram.model.Train;
 import com.example.planergram.repository.TrainRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,14 +16,28 @@ public class TrainService {
 
     private TrainRepository trainRepository;
 
-    public List<Train> findAll(){
-        return trainRepository.findAll();
+    @Transactional
+    public List<TrainDTO> getTrainList(){
+        List<Train> Trains = trainRepository.findAll();
+        List<TrainDTO> TrainDTOList = new ArrayList<>();
+
+        for(Train train : Trains){
+            TrainDTO dto = TrainDTO.builder()
+                    .trainId(train.getTrainId())
+                    .departureTime(train.getDepartureTime())
+                    .arriveTime(train.getArriveTime())
+                    .startPoint(train.getStartPoint())
+                    .endPoint(train.getEndPoint())
+                    .build();
+            TrainDTOList.add(dto);
+        }
+        return TrainDTOList;
     }
 
-
-    public List<Train> findRegion(String endPoint) {
-        return trainRepository.findBySelected(endPoint);
-    }
+    //보류
+//    public List<Train> findRegion(String endPoint) {
+//        return trainRepository.findBySelected(endPoint);
+//    }
 
 
 }
