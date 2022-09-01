13f8e343c1768996306d73b7584558468005c6b7
Yiseun <yiseunn@gmail.com>
2022-09-01 17:34:05 +0900

    refactor: Train service 전체조회기능 수정 및 어노테이션 추가

--- src/main/java/com/example/planergram/service/TrainService.java
+++ src/main/java/com/example/planergram/service/TrainService.java
@@ -4,7 +4,10 @@ import com.example.planergram.DTO.AttractionDTO;
 import com.example.planergram.DTO.TrainDTO;
 import com.example.planergram.model.Attraction;
 import com.example.planergram.model.Train;
+import com.example.planergram.repository.AttractionRepository;
 import com.example.planergram.repository.TrainRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +16,15 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class TrainService {
 
+    @Autowired
     private TrainRepository trainRepository;
+
+    private TrainDTO trainDTO;
+
+    private Train train;
 
     @Transactional(readOnly = true)
     public List<TrainDTO> getTrainList(){
@@ -35,39 +44,7 @@ public class TrainService {
         return TrainDTOList;
     }
 
-    @Transactional
-    public void deleteTrain(Long trainId){
-        Optional<Train> optTrain = trainRepository.findById(trainId);
-        if(optTrain.isPresent()){
-            Train train = optTrain.get();
-            trainRepository.deleteById(trainId);
-        }
-    }
 
-    public void createTrain(TrainDTO trainDTO){
-        Train train = Train.builder()
-                .trainId(trainDTO.getTrainId())
-                .departureTime(trainDTO.getDepartureTime())
-                .arriveTime(trainDTO.getArriveTime())
-                .startPoint(trainDTO.getStartPoint())
-                .endPoint(trainDTO.getEndPoint())
-                .build();
-        trainRepository.save(train);
-    }
-
-    public Train editTrain(TrainDTO trainDTO){
-        try{
-            Train train = trainRepository.findById(trainDTO.getTrainId()).get();
-            train.setDepartureTime(trainDTO.getDepartureTime());
-            train.setArriveTime(trainDTO.getArriveTime());
-            train.setStartPoint(trainDTO.getStartPoint());
-            train.setEndPoint(trainDTO.getEndPoint());
-            return trainRepository.save(train);
-        }catch (Exception e){
-            e.printStackTrace();
-            return null;
-        }
-    }
 
     //보류
 //    public List<Train> findRegion(String endPoint) {
