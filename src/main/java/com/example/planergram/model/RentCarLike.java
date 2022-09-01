96f7d709b23bc9057bd2b8399f023fc1c98e1ba8
Glory <dudrhkd6550@naver.com>
2022-09-01 22:55:54 +0900

    feat: RentCar&RentCarLike 엔티티생성

---
+++ src/main/java/com/example/planergram/model/RentCarLike.java
@@ -0,0 +1,29 @@
+package com.example.planergram.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "RentCar_Like")
+public class RentCarLike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "rentCar_like_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = User.class)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = RentCar.class)
+    @JoinColumn(name = "rentCar_id")
+    private RentCar rentCar;
+}
