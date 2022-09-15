714e7ecd898fc504cfd6326ab0d27f94ce70579e
소울치킨 <soulfever01@naver.com>
2022-09-15 16:16:40 +0900

    feat : principalDetails, Service 구현

---
+++ src/main/java/com/example/planergram/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,24 @@
+package com.example.planergram.config.auth;
+
+import com.example.planergram.user.model.User;
+import com.example.planergram.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+// /login요청이 올때 동작을 한다.
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("PrincipalDetailsService의 loadUserByUsername()");
+        User userEntity = userRepository.findByLoginId(username);
+        return new PrincipalDetails(userEntity);
+    }
+}
