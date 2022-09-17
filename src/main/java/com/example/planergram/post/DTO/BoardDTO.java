e3b1159798c0e4c8e07dd82a2eb153018461ab5b
소울치킨 <soulfever01@naver.com>
2022-09-18 01:25:43 +0900

    feat : BoardDTO에 ApiModelProperty 적용

--- src/main/java/com/example/planergram/post/DTO/BoardDTO.java
+++ src/main/java/com/example/planergram/post/DTO/BoardDTO.java
@@ -1,5 +1,6 @@
 package com.example.planergram.post.DTO;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,8 +14,14 @@ import java.util.List;
 @Builder
 public class BoardDTO {
 
+    @ApiModelProperty(value = "게시판의 ID", example = "1")
     private Long boardId;
+    @ApiModelProperty(value = "게시판의 이르", example = "서울")
     private String boardTitle;
+
+    @ApiModelProperty(value = "게시판의 이미지", example = "soul.jpg")
     private String img;
+
+    @ApiModelProperty(hidden = true)
     private List<Long> postDTOList;
 }
