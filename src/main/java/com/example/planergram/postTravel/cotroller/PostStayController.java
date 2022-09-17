3d8dfe45a51a159114755f4d7c7dcf900029558f
소울치킨 <soulfever01@naver.com>
2022-09-18 03:22:34 +0900

    feat : PostStayController Swagger 적용

--- src/main/java/com/example/planergram/postTravel/cotroller/PostStayController.java
+++ src/main/java/com/example/planergram/postTravel/cotroller/PostStayController.java
@@ -2,21 +2,33 @@ package com.example.planergram.postTravel.cotroller;
 
 import com.example.planergram.Response.ResponseService;
 import com.example.planergram.postTravel.service.PostStayService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/poststay")
+@RequestMapping("api")
+@Api(tags = {"게시글에 있는 여행경로(숙소) API 정보를 제공하는 Controller"})
 public class PostStayController {
 
     @Autowired
     private PostStayService postStayService;
 
-    @GetMapping
+    private final String VERSION = "/v1";
+    private final String AUTH = "/auth" + VERSION;
+    private final String ADMIN_AUTH = "/admin" + AUTH + VERSION;
+    private final String POST_CONTENTS = "/post-contents";
+    private final String POST_STAY = POST_CONTENTS + "/post-stay";
+
+
+    @GetMapping(AUTH + POST_STAY)
+    @ApiOperation(value = "USER : 해당 게시글에 숙소를 추가하는 API")
     public ResponseEntity<?> clickPostStay(
-            @RequestParam(value="post", defaultValue="0") Long postId,
-            @RequestParam(value="stay", defaultValue="0") Long stayId){
+            @ApiParam(value = "게시글의 ID값") @RequestParam(value="post", defaultValue="0") Long postId,
+            @ApiParam(value = "숙소의 ID값") @RequestParam(value="stay", defaultValue="0") Long stayId){
         try {
             return ResponseEntity.ok(postStayService.clickStayLike(postId, stayId));
         } catch (Exception e) {
@@ -24,8 +36,9 @@ public class PostStayController {
         }
     }
 
-    @GetMapping("/post/{postId}")
-    public ResponseEntity<?> postStayFindByPost(@PathVariable Long postId){
+    @GetMapping(AUTH + "/post/{postId}" + POST_STAY)
+    @ApiOperation(value = "USER : 해당 게시글에 숙소를 조회하는 API")
+    public ResponseEntity<?> postStayFindByPost(@ApiParam(value = "게시글의 ID값") @PathVariable Long postId){
         try {
             return ResponseEntity.ok(postStayService.findByPost(postId));
         } catch (Exception e) {
@@ -33,8 +46,9 @@ public class PostStayController {
         }
     }
 
-    @GetMapping("/stay/{stayId}")
-    public ResponseEntity<?> postStayFindByStay(@PathVariable Long stayId){
+    @GetMapping(ADMIN_AUTH + POST_STAY + "/stay/{stayId}")
+    @ApiOperation(value = "ADMIN : 숙소를 게시글에 추가한 경우를 숙소로 조회하는 API")
+    public ResponseEntity<?> postStayFindByStay(@ApiParam(value = "숙소의 ID값") @PathVariable Long stayId){
         try {
             return ResponseEntity.ok(postStayService.findByStay(stayId));
         } catch (Exception e) {
@@ -42,8 +56,9 @@ public class PostStayController {
         }
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable Long id){
+    @GetMapping(ADMIN_AUTH + POST_STAY + "/{id}")
+    @ApiOperation(value = "ADMIN : 숙소를 게시글에 추가한 경우를 ID로 조회하는 API")
+    public ResponseEntity<?> findById(@ApiParam(value = "포스트 스테이의 ID값") @PathVariable Long id){
         try {
             return ResponseEntity.ok(postStayService.findById(id));
         } catch (Exception e) {
